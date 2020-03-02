27e955b727d7d7b0563a0a6e9c2309cd9552fdeb
wangliang <wg_lg520@21cn.com>
2020-03-02 12:35:26 +0800

    commit by wangliang

--- src/main/java/com/generator/code/controller/TestController.java
+++ src/main/java/com/generator/code/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.generator.code.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.generator.code.dao.SUserRepo;
+import com.generator.code.entity.SUserEntity;
 import com.generator.code.freemarker.FreemarkerUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.support.MetaDataAccessException;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -18,11 +22,16 @@ public class TestController {
     @Resource
     FreemarkerUtil freemarkerUtil;
 
+    @Resource
+    SUserRepo sUserRepo;
+
     @RequestMapping(value = "/conn", method = RequestMethod.GET)
 //    @GetMapping("/conn")
     public void conn() throws IOException, MetaDataAccessException {
         log.info("welcome conn");
-        freemarkerUtil.createCode();
+//        freemarkerUtil.createCode();
+        List<SUserEntity> list = sUserRepo.findAll();
+        log.info("list:{}", JSONObject.toJSONString(list));
 //        return null;
     }
 }
